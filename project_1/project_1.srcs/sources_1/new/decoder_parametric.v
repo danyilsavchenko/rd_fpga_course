70d942eaba2af3ff3941406630147e7ee726d6a5
Danyil <danchiksavchik@gmail.com>
2026-08-29 16:59:38 +0300

    Initial commit

---
+++ project_1/project_1.srcs/sources_1/new/decoder_parametric.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/29/2026 04:54:07 PM
+// Design Name: 
+// Module Name: decoder_parametric
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module decoder_parametric(
+
+    );
+endmodule
